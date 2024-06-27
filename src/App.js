65bc95eca532d0a7a9d99f1a2829519f9137906b
fuad <ahmadufuadoorakam@gmail.com>
2024-06-27 06:58:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from './useForm';
+import { useEffect } from 'react'
 
 function App() {
+
+  const [value, handleChange] = useForm({
+    name: "",
+    email: ""
+  })
+
+  useEffect(() => {
+    console.log("Running");
+    return () => {
+      console.log("Unmount")
+    }
+  }, [value.name])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form action="">
+        <input type="text" name='name' value={value.name} placeholder='Name' onChange={handleChange} />
+        <input type="email" name='email' value={value.email} placeholder='Email' onChange={handleChange} />
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
