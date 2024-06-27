46d6b788db60ee539812eedf5ec45e8befe5e4fe
fuad <ahmadufuadoorakam@gmail.com>
2024-06-27 07:10:31 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,47 @@
-import { useForm } from './useForm';
-import { useEffect } from 'react'
+import { useForm } from "./useForm";
+import { useEffect } from "react";
 
 function App() {
-
   const [value, handleChange] = useForm({
     name: "",
-    email: ""
-  })
+    email: "",
+    password: "",
+  });
 
   useEffect(() => {
     console.log("Running");
     return () => {
-      console.log("Unmount")
-    }
-  }, [value.name])
-
-
+      console.log("Unmount");
+    };
+  }, [value.name]);
 
   return (
     <div>
       <form action="">
-        <input type="text" name='name' value={value.name} placeholder='Name' onChange={handleChange} />
-        <input type="email" name='email' value={value.email} placeholder='Email' onChange={handleChange} />
+        <input
+          type="text"
+          name="name"
+          value={value.name}
+          placeholder="Name"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          name="email"
+          value={value.email}
+          placeholder="Email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={value.password}
+          placeholder="Password"
+          onChange={handleChange}
+        />
       </form>
     </div>
-  )
+  );
 }
 
 export default App;
