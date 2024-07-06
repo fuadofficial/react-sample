863b0175ab7676731f55d2696402f88993cb1625
fuad <ahmadfuadoorakam@gmail.com>
2024-07-06 14:58:15 +0530

    useEffect

--- src/Counter.js
+++ src/Counter.js
@@ -1,8 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-function Counter({ increment, decrement }) {
+function Counter() {
   const [value, setValue] = useState(0);
   const [color, setColor] = useState("white");
+  const [boom, setBoom] = useState(false);
+
+  useEffect(() => {
+    setBoom(false)
+    const id = setTimeout(() => {
+      setBoom(true);
+    }, value * 1000);
+    return () => {
+      clearTimeout(id);
+    };
+  }, [value]);
 
   return (
     <div style={{ backgroundColor: color }}>
@@ -14,6 +25,10 @@ function Counter({ increment, decrement }) {
       <br />
       <button onClick={() => setColor("green")}>Green</button>
       <button onClick={() => setColor("red")}>Red</button>
+      <br />
+      <br />
+      <br />
+      <div>{boom && value ? <h1>Boom</h1> : null}</div>
     </div>
   );
 }
---
+++ src/Effect.js
@@ -0,0 +1,8 @@
+import React, { useEffect } from "react";
+
+function Effect() {
+  
+  return <div>Effect</div>;
+}
+
+export default Effect;
